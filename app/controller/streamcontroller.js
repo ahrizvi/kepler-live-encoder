e9fc87b297958fcb460f2b423aa9cd75a3143e17
Anfal Haider <anfalrizvi@gmail.com>
2019-10-13 17:50:32 +0500

    add stream live check

--- app/controller/streamcontroller.js
+++ app/controller/streamcontroller.js
@@ -50,51 +50,55 @@ exports.ListDir = (req, res) => {
 }
 
 exports.StartStream = (req, res) => {
-        console.log('startstream');
+    console.log('startstream');
 
-        Asset.findOne({
-	where: { id: req.params.id },
-            //attributes: []
-        }).then(startstream => {
+    Asset.findOne({
+        where: { id: req.params.id },
+        //attributes: []
+    }).then(startstream => {
             if (!startstream) {
                 return res.status(404).json({ "error": "Can Not Start Stream, Invalid Asset ID" });
             }
-            const input = "udp://239.195.4.3:5000/"
-	    const output = "udp://10.100.40.15:7777/"	
-	
-	    const { spawn } = require('child_process');
-            const ffmpeg = spawn('ffmpeg', [
-                "-loglevel", "error",
-                "-re",
-                "-i", input,
-                "-vcodec", "h264",
-                "-r", "25",
-                "-flags", "cgop+ilme",
-                "-sc_threshold", "1000000000",
-                "-b:v", "1.7M",
-                "-minrate:v", "1.5M",
-                "-maxrate:v", "2.5M",
-                "-bufsize:v", "5M",
-                "-acodec", "aac",
-                "-ac", "2",
-                "-b:a", "64k",
-                "-f", "mpegts", output
-            ], {
-                detached: true
-            });
-            var procid = ffmpeg.pid
-            console.log(procid)
-            res.status(200).json({
-                "description": "Asset has been started successfully",
-                "Result": 'Asset with PID' + ' ' + procid + ' ' + 'has been started'
-            });
- 	           
-	      var dummy = 'dummy1'
-	      console.log(dummy);
+            var streamIsActive = startstream.active
+            if (!streamIsActive == '0')
+                return res.status(400).json({ "error": "Stream is already Live" });
+        }
 
-	      ffmpeg.stderr.on('data', (err) => {
-                            console.log('err:', new String(err))
-                        })
-	})
- 
-}	
+        const input = "udp://239.195.4.3:5000/"
+        const output = "udp://10.100.40.15:7777/"
+
+        const { spawn } = require('child_process');
+        const ffmpeg = spawn('ffmpeg', [
+            "-loglevel", "error",
+            "-re",
+            "-i", input,
+            "-vcodec", "h264",
+            "-r", "25",
+            "-flags", "cgop+ilme",
+            "-sc_threshold", "1000000000",
+            "-b:v", "1.7M",
+            "-minrate:v", "1.5M",
+            "-maxrate:v", "2.5M",
+            "-bufsize:v", "5M",
+            "-acodec", "aac",
+            "-ac", "2",
+            "-b:a", "64k",
+            "-f", "mpegts", output
+        ], {
+            detached: true
+        });
+        var procid = ffmpeg.pid
+        console.log(procid) res.status(200).json({
+            "description": "Asset has been started successfully",
+            "Result": 'Asset with PID' + ' ' + procid + ' ' + 'has been started'
+        });
+
+        var dummy = 'dummy1'
+        console.log(dummy);
+
+        ffmpeg.stderr.on('data', (err) => {
+            console.log('err:', new String(err))
+        })
+    })
+
+}
