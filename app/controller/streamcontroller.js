733666addce86bacae316697938140df16d56f1e
Anfal Haider <anfalrizvi@gmail.com>
2019-10-13 15:57:04 +0500

    modifications in Asset Start Controller

--- app/controller/streamcontroller.js
+++ app/controller/streamcontroller.js
@@ -49,56 +49,64 @@ exports.ListDir = (req, res) => {
     })
 }
 
-exports.StreamStart = (req, res) => {
-    console.log('streamstart');
-    var procid = req.body.procid
-     if (!procid === '1') {
-         console.log(ffmpeg.pid)}
-   
-    
-    var input = "udp://239.195.4.3:5000/"
-    var output = "udp://10.100.40.15:7777/"
-    const { spawn } = require('child_process');
-	
-    const ffmpeg = spawn('ffmpeg', [
-        "-loglevel", "error",
-	"-re",
-        "-i", input,
-        "-vcodec","h264",
-        "-r", "25",
-        "-flags", "cgop+ilme",
-        "-sc_threshold", "1000000000",
-        "-b:v", "1.7M",
-        "-minrate:v", "1.5M",
-        "-maxrate:v", "2.5M",
-        "-bufsize:v", "5M",
-        "-acodec", "aac",
-        "-ac", "2",
-        "-b:a", "64k",
-        "-f", "mpegts", output
-    ], {
-    detached: true });
-     var procid = req.body.procid
-     console.log(procid)
-	
-	// if (procid === '1') {
-  	// console.log(ffmpeg.pid)}else{console.log('procid != 1')};
-	
-	res.status(200).json({
-            "description": "Param 1",
-            "Result": "Channel has been started"
-        });
+exports.StartStream = (req, res) => {
+        console.log('startstream');
 
-ffmpeg.on('exit', (statusCode) => {
-  if (!statusCode === 0) {
-     console.log('Process Crashed')
-}
-})
+        Asset.findOne({
+            where: { id: req.params.id },
+            //attributes: []
+        }).then(startstream => {
+            if (!startstream) {
+                return res.status(404).json({ "error": "Can Not Start Stream, Invalid Asset ID" });
+            }
+            const { spawn } = require('child_process');
+            const ffmpeg = spawn('ffmpeg', [
+                "-loglevel", "error",
+                "-re",
+                "-i", input,
+                "-vcodec", "h264",
+                "-r", "25",
+                "-flags", "cgop+ilme",
+                "-sc_threshold", "1000000000",
+                "-b:v", "1.7M",
+                "-minrate:v", "1.5M",
+                "-maxrate:v", "2.5M",
+                "-bufsize:v", "5M",
+                "-acodec", "aac",
+                "-ac", "2",
+                "-b:a", "64k",
+                "-f", "mpegts", output
+            ], {
+                detached: true
+            });
+            var procid = ffmpeg.pid
+            console.log(procid)
+            res.status(200).json({
+                "description": "Asset has been started successfully",
+                "Result": 'Asset with PID' + ' ' + procid + ' ' + 'has been started'
+            });
 
-ffmpeg
-  .stderr
-  .on('data', (err) => {
-    console.log('err:', new String(err))
-  })	
-	
-}
+        }).catch(err => {
+                ffmpeg.on('exit', (statusCode) => {
+                    if (!statusCode === 0) {
+                        console.log('Process Crashed')
+
+                        res.status(500).json({
+                                "description": "Process Crashed"
+                                "error": 'Process Crashed'
+                            }
+                        })
+
+                    ffmpeg
+                        .stderr
+                        .on('data', (err) => {
+                            console.log('err:', new String(err))
+                        })
+
+                    res.status(500).json({
+                        "description": "Asset can not be started due to an error. Please check logs",
+                        "error": err.message
+                    });
+                })
+            }
+        }
