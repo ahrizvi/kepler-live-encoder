e0efaf52ad7a3035c292509fdb73dc9b23d187fe
Anfal Haider <anfalrizvi@gmail.com>
2019-10-14 19:57:38 +0500

    bug fix

--- app/controller/streamhealth.js
+++ app/controller/streamhealth.js
@@ -10,3 +10,23 @@ exports.index = (req, res) => {
         "Page": "Index"
     });
 }
+
+exports.AssetStatusAll = (req, res) => {
+        console.log('assetstatusall');
+
+        Asset.findAll({
+                //  where: {id: 1},
+                attributes: ['name', 'active', =
+
+                }).then(assetlistall => {
+                res.status(200).json({
+                    "description": "Asset List",
+                    "Result": assetlistall
+                });
+            }).catch(err => {
+                res.status(400).json({
+                    "description": "Can not update Asset",
+                    "error": err.message
+                });
+            })
+        }
