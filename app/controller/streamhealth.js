d359b3edc9bc43f14074afbee5019242c9ca9751
Anfal Haider <anfalrizvi@gmail.com>
2019-10-17 00:13:28 +0500

    asset health module test

--- app/controller/streamhealth.js
+++ app/controller/streamhealth.js
@@ -39,6 +39,36 @@ exports.AssetStatusAll = (req, res) => {
     }).map(el => el.get('id')).then(assetlistall => {
 
         activeAssetArr = assetlistall
+
+        var ps = require('ps-node');
+        ps.lookup({ pid: 123 },
+            function(err, resultList) {
+                if (err) {
+                    throw new Error(err);
+                }
+
+                assetlistall.forEach(function(process) {
+                    if (process) {
+
+                        //   console.log('PID: %s, COMMAND: %s, ARGUMENTS: %s', process.pid, process.command, process.arguments);
+                        sys_proc_cmd = process.command
+                        sys_proc_args = process.arguments
+
+
+                        db_input_loc = "udp://239.195.4.3:5000/"
+                        db_proc_cmd = "ffmpeg"
+
+                        var chk1 = sys_proc_args.includes(db_input_loc)
+                        var chk2 = sys_proc_cmd.includes(db_proc_cmd)
+                        var chk3 = chk1 + ' ' + chk2
+                        console.log(chk3);
+
+                    } else {
+                        console.log('No such process found!');
+                    }
+                });
+            });
+
         activeAssetArr.forEach(element => {
 
 
@@ -51,32 +81,76 @@ exports.AssetStatusAll = (req, res) => {
                 var assetname = assetlistone.name
                 var procid = assetlistone.output_nix_procid
                 var inloc = assetlistone.input_location
-		console.log(procid);	
+                console.log(procid);
+            })
 
- 		var ps = require('ps-node');               
-		ps.lookup({ pid: 123 },
+        })
+
+        //  })
+
+        res.status(200).json({
+            "description": "Asset List",
+            "Result": "assetlistall"
+        });
+        // }
+
+
+        //	.catch(err => {
+        //        res.status(400).json({
+        //            "description": "Can not update Asset",
+        //           "error": err.message
+        //       });
+        //  })
+    })
+
+}
+
+exports.AssetStatusAllBkp = (req, res) => {
+
+    Asset.findAll({
+        where: { active: 1 }
+
+    }).map(el => el.get('id')).then(assetlistall => {
+
+        activeAssetArr = assetlistall
+        activeAssetArr.forEach(element => {
+
+
+            Asset.findOne({
+                where: { id: element },
+                attributes: ['id', 'name', 'input_location', 'output_nix_procid']
+            }).then(assetlistone => {
+
+                var assetid = assetlistone.id
+                var assetname = assetlistone.name
+                var procid = assetlistone.output_nix_procid
+                var inloc = assetlistone.input_location
+                console.log(procid);
+
+                var ps = require('ps-node');
+                ps.lookup({ pid: 123 },
                     function(err, resultList) {
                         if (err) {
                             throw new Error(err);
-                       }
+                        }
 
                         resultList.forEach(function(process) {
                             if (process) {
 
-                             //   console.log('PID: %s, COMMAND: %s, ARGUMENTS: %s', process.pid, process.command, process.arguments);
+                                //   console.log('PID: %s, COMMAND: %s, ARGUMENTS: %s', process.pid, process.command, process.arguments);
                                 sys_proc_cmd = process.command
                                 sys_proc_args = process.arguments
 
 
-                                  db_input_loc = "udp://239.195.4.3:5000/"
-                                  db_proc_cmd = "ffmpeg"
+                                db_input_loc = "udp://239.195.4.3:5000/"
+                                db_proc_cmd = "ffmpeg"
 
-                                  var chk1 = sys_proc_args.includes(db_input_loc)
-                                  var chk2 = sys_proc_cmd.includes(db_proc_cmd)
-                                  var chk3 = chk1+' '+chk2
-				  console.log(chk3);
+                                var chk1 = sys_proc_args.includes(db_input_loc)
+                                var chk2 = sys_proc_cmd.includes(db_proc_cmd)
+                                var chk3 = chk1 + ' ' + chk2
+                                console.log(chk3);
 
-                            }else{
+                            } else {
                                 console.log('No such process found!');
                             }
                         });
@@ -86,21 +160,21 @@ exports.AssetStatusAll = (req, res) => {
 
         })
 
-   // })
+        // })
 
-    res.status(200).json({
-        "description": "Asset List",
-        "Result": "assetlistall"
-    });
-    // }
+        res.status(200).json({
+            "description": "Asset List",
+            "Result": "assetlistall"
+        });
+        // }
 
 
-    //	.catch(err => {
-    //        res.status(400).json({
-    //            "description": "Can not update Asset",
-    //           "error": err.message
-    //       });
-    //  })
-})
+        //	.catch(err => {
+        //        res.status(400).json({
+        //            "description": "Can not update Asset",
+        //           "error": err.message
+        //       });
+        //  })
+    })
 
 }
